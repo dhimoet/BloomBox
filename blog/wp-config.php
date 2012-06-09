864d0982b721726941420a9c0b2d8bf1d4f53752
Adhika Mahendra <dhimoet@gmail.com>
2012-06-09 16:35:29 -0400

    blah

--- blog/wp-config.php
+++ blog/wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'bloomboxphoto');
 
 /** MySQL database username */
-define('DB_USER', 'bloomboxphoto');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'QazWsxEdc123');
+define('DB_PASSWORD', 'password');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
