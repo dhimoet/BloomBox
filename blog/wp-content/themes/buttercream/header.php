832ea395ddbb38ba0d4f32913860e9c99648aede
Adhika Mahendra <dhimoet@gmail.com>
2012-06-09 18:30:51 -0400

    added google anlytics

--- blog/wp-content/themes/buttercream/header.php
+++ blog/wp-content/themes/buttercream/header.php
@@ -39,6 +39,18 @@
 <link rel="stylesheet" type="text/css" media="all" href="<?php echo get_template_directory_uri(); ?>/layouts/ie.css"/>
 <![endif]-->
 <?php wp_head(); ?>
+    <script type="text/javascript">
+        /*** Google Analytics ***/
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-32531428-1']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
 </head>
 
 <body <?php body_class(); ?>>
