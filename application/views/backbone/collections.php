2ec7412fce15845aaf86cef13f34e694f6761178
Adhika Mahendra <dhimoet@gmail.com>
2012-08-21 10:38:35 -0400

    backbone

---
+++ application/views/backbone/collections.php
@@ -0,0 +1,5 @@
+<script>
+	var content_collection = new Backbone.Collection.extend({
+		model: content_model,
+	});
+</script>
---
+++ application/views/backbone/models.php
@@ -0,0 +1,10 @@
+<script>
+	var content_model = Backbone.Model.extend({
+		defaults: {
+			image: "/public/img/covers/cover_image_welcome.png",
+			frame: "/public/img/covers/blue_frame.png",
+			text: "",
+			bottom: "",
+		}
+	});
+</script>
